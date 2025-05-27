848535acfaafc0a2bbb4cd147063fc6471f2b880
Nicolas <njcaceres04@gmail.com>
2025-05-26 22:46:50 -0500

    functional toggle button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+import ToggleMode from "@/components/toggleMode";
 export default function Home() {
-  return <div className="h-screen flex justify-center items-center">Hola</div>;
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <ToggleMode />
+    </div>
+  );
 }
